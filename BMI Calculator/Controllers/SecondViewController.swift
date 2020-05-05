603502a30c6ed3722b84f8e5c4711cbeec278f41
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-05 13:55:45 -0400

    Create SecondViewController

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Iurie Guzun on 2020-05-05.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel()
+        label.text = "Hello"
+    }
+}
