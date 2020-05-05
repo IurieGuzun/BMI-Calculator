39e58e1527680360f264e314e5fdb51b5294ceee
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-04 21:45:28 -0400

    Configure Calculate Button

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
           super.viewDidLoad()
-          // Do any additional setup after loading the view.
+        
       }
     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        print(String(format: "%.2f", sender.value))
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-         print(sender.value)
+         print(String(format: "%.0f", sender.value))
+         weightLabel.text = String(format: "%.0f", sender.value) + "Kg"
     }
     
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(heightSlider.value)
+        let height = heightSlider.value
+        print(weightSlider.value)
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 
 }
 
